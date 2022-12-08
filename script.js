97526189e8a55b6ffff188d59247d4efae05ad7c
Glitch (immediate-complete-ironclad) <none>
2022-12-08 22:09:00 +0000

    🎀⛺️ Checkpoint ./style.css:29805441/76435 ./script.js:29805441/93944 ./index.html:29805441/21121

--- script.js
+++ script.js
@@ -691,19 +691,3 @@ var snowStorm = (function(window, document) {
   return this;
 
 }(window, document));
-(function($) {
-    $(document).ready(function() {
-        $.ajax({
-            url: 'https://randomuser.me/api?inc=name,picture,email,phone&nat=us&results=6&noinfo',
-            dataType: 'json'
-        }).done(function(data) {
-            var html = $('#cardProfileTemplate').html(),
-                template = Handlebars.compile(html),
-                result = template(data);
-            
-            $('#profileCardGrid').append(result);
-        }).fail(function(msg) {
-            alert('Ajax failed with status: ' + msg); 
-        });
-    });
-})(jQuery);
