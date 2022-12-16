d3bd0f6f071ea3600f51432df0e237324e0a3d05
Glitch (immediate-complete-ironclad) <none>
2022-12-16 21:15:57 +0000

    💝💂 Checkpoint ./script.js:29805441/42736 ./index.html:29805441/42439 ./style.css:29805441/70452

--- script.js
+++ script.js
@@ -476,7 +476,7 @@ function SnowStorm() {
       ? [setInterval(s.snow, s.animationInterval)]
       : [
           setInterval(s.snow, s.animationInterval * 3),
-          setInterval(s.snow, s.animationInterval),
+          setInterval(s.snow, s.animationInterval)
         ];
   };
 
@@ -593,22 +593,4 @@ This is a comment that can span multiple lines
 /*jslint nomen: true, plusplus: true, sloppy: true, vars: true, white: true */
 /*global window, document, navigator, clearInterval, setInterval */
 
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  } // Show the current tab, and add an "active" class to the button that opened the tab
-
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
